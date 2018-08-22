3316d076a9fff2bf657b6b2ec2b0fd0497bdb561
mwkkk <234845837@qq.com>
2018-08-22 21:58:55 +0800

    webpack-initial

---
+++ src/app.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import 'font-awesome/css/font-awesome.min.css'
+import './index.css'
+import './index.scss'
+
+ReactDOM.render(
+    <div>
+        <i className="fa fa-bath"></i> 
+        here333333333333333333333333 
+    </div>,
+    document.getElementById('app')
+)
